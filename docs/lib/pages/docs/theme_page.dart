eea7beab2b284e3b755d6f11e8b31a2967ce8e4b
M Abrar Javed <abrar.javed3@gmail.com>
2026-03-02 03:51:32 +0000

    fix(theme): avoid controller mutation during build init

--- docs/lib/pages/docs/theme_page.dart
+++ docs/lib/pages/docs/theme_page.dart
@@ -156,11 +156,6 @@ class _ThemePageState extends State<ThemePage> {
       );
     }
 
-    if (controller.presetId != 'custom' &&
-        controller.presetId != _basePresetId) {
-      controller.setPreset(_basePresetId);
-    }
-
     _presetSelectionsReady = true;
   }
 
@@ -393,7 +388,7 @@ class _ThemePageState extends State<ThemePage> {
   Widget _buildKitchenPaymentMethod(BuildContext context) {
     final currentYear = DateTime.now().year;
     return SurfaceCard(
-      padding: const EdgeInsetsDensity.all(padLg),
+      padding: const EdgeInsetsDensity.all(pad3xl),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
